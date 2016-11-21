57b2a3728ddc9be9016218cc26f16afa970a468d
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2016-11-21 16:46:53 +0200

    implement guassian nb to determine author of emails from enron

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,8 +28,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
+t0 = time()
+fitting = clf.fit(features_train,labels_train)
+train_time = round(time()-t0, 3)
+print "training time:", train_time, "s"
 
+t0 = time()
+predictions = clf.predict(features_test)
+pred_time = round(time()-t0, 3)
+print "prediction time:", pred_time, "s"
 
+accuracy = accuracy_score(labels_test, predictions)
+print "Accuracy: %s  Training Time: %ss  Prediction Time: %ss" %(accuracy, train_time, pred_time)
 #########################################################
 
 
+'''
+t0 = time()
+< your clf.fit() line of code >
+print "training time:", round(time()-t0, 3), "s"
+'''
